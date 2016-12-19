e8bd7603529bf37d15886cb3aa15df249980a812
Jugal Kishor <jugal.ionic@gmail.com>
2016-12-20 02:15:02 +0530

    new build

--- server-app/server/server.js
+++ server-app/server/server.js
@@ -42,7 +42,7 @@ var users = [];
 var sockets = {};
 
 app.use((0, _compression2.default)({}));
-app.use(_express2.default['static'](__dirname + '/../extension/lib'));
+app.use(_express2.default['static'](__dirname + '/../client'));
 // allow CORS
 app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
