2507f6d42987b245a4de5236fb80759cd2525afe
jugal-ionic <jugal.ionic@gmail.com>
2016-12-28 07:24:10 +0530

    a

--- extension/js/tabs.js
+++ extension/js/tabs.js
@@ -1,10 +1,17 @@
 function loadContentScriptInAllTabs() {
   chrome.windows.getAll({'populate': true}, function(windows) {
-        chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    for (var i = 0; i < windows.length; i++) {
+      var tabs = windows[i].tabs;
+      for (var j = 0; j < tabs.length; j++) {
         chrome.tabs.executeScript(
-            tabId,
-            {file: 'window_side_script.js', allFrames: true});
-          }); 
-          
+            tabs[j].id,
+            {file: './window_side_script.js', allFrames: true});
+      }
+    }
   });
 }
+//chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+//        chrome.tabs.executeScript(
+//            tabId,
+//            {file: 'window_side_script.js', allFrames: true});
+//          }); 
